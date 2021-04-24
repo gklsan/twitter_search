a8aa904b6dc84d17f125bb68acee19eeca1eea04
Gokul <pgokulmca@gmail.com>
2021-04-24 10:19:19 +0530

    Search controller updated

--- app/controllers/twitter_search_controller.rb
+++ app/controllers/twitter_search_controller.rb
@@ -1,4 +1,16 @@
 class TwitterSearchController < ApplicationController
   def search
+    @result = twitter_client.search(params[:seach_text], {count: 50}).take(100) if params[:seach_text].present?
+  end
+
+  private
+
+  def twitter_client
+    @twitter_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_SECRET']
+    end
   end
 end
